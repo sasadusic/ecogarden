2ce6e2a3a6ae91c7c79869377a6c53a9919e9530
sasadusic <sasadusic01101989@gmail.com>
2025-05-31 21:55:46 +0200

    3 commit

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-const height = window.innerHeight
+const nav = document.getElementById('nav')
+const navLinks = document.querySelectorAll('.nav-link')
+const lightColor = '#ececec'
+const darkColor = '#2e2e2e'
 
-document.addEventListener('scroll', () => {
-    
+window.addEventListener("scroll", () => {
+    console.log('test')
+    const scrollY = window.scrollY
+    const windowHeight = window.innerHeight
+
+    let scrollRatio = Math.min(scrollY / windowHeight, 1)
+    nav.style.backgroundColor = `rgba(1, 86, 29, ${scrollRatio})`
+
+    if(scrollRatio > .5){
+        nav.style.color = lightColor
+        navLinks.forEach(link => link.style.color = lightColor)
+    }
+    else{
+        nav.style.color = darkColor
+        navLinks.forEach(link => link.style.color = darkColor)
+        
+    }
 })
