638ac29de956420f5b440ef18f853be0110d6f88
sasadusic <sasadusic01101989@gmail.com>
2025-06-03 16:16:55 +0200

    3 commit

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 const nav = document.getElementById('nav')
+const hamburger = document.getElementById('hamburger')
 const navLinks = document.querySelectorAll('.nav-link')
 const lightColor = '#ececec'
 const darkColor = '#2e2e2e'
 
 window.addEventListener("scroll", () => {
-    console.log('test')
     const scrollY = window.scrollY
+    const windowWidth = window.innerWidth
     const windowHeight = window.innerHeight
 
-    let scrollRatio = Math.min(scrollY / windowHeight, 1)
-    nav.style.backgroundColor = `rgba(1, 86, 29, ${scrollRatio})`
+    if(windowWidth > 992){
 
-    if(scrollRatio > .5){
-        nav.style.color = lightColor
-        navLinks.forEach(link => link.style.color = lightColor)
-    }
-    else{
-        nav.style.color = darkColor
-        navLinks.forEach(link => link.style.color = darkColor)
         
+        let scrollRatio = Math.min(scrollY / windowHeight, 1)
+        nav.style.backgroundColor = `rgba(24, 78, 77, ${scrollRatio})`
+        
+        if(scrollRatio > .5){
+            nav.style.color = lightColor
+            navLinks.forEach(link => link.style.color = lightColor)
+        }
+        else{
+            nav.style.color = darkColor
+            navLinks.forEach(link => link.style.color = darkColor)
+            
+        }
     }
 })
+hamburger.onclick = () => {
+    hamburger.classList.toggle('active')
+    nav.classList.toggle('active')
+}
